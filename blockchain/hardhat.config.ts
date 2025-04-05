27def738dd49dad68d85d30cfddf50c5461ffe8f
Alan0817 <alan90817@gmail.com>
2025-04-06 06:18:35 +0800

    clean config

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -1,7 +1,13 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 import 'hardhat-multibaas-plugin';
+import { config as dotEnvConfig } from 'dotenv';
 import path from 'path';
+import '@nomicfoundation/hardhat-toolbox';
+import '@nomicfoundation/hardhat-verify';
+
+
+dotEnvConfig();
 
 let deployerPrivateKey = '0x0000000000000000000000000000000000000000000000000000000000000000';
 let deploymentEndpoint = '';
@@ -30,12 +36,47 @@ const config: HardhatUserConfig = {
       url: web3Url,
       accounts: [deployerPrivateKey],
     },
+    alfajores: {
+      accounts: [process.env.PRIVATE_KEY ?? '0x0'],
+      url: 'https://alfajores-forno.celo-testnet.org',
+    },
+    celo: {
+      accounts: [process.env.PRIVATE_KEY ?? '0x0'],
+      url: 'https://forno.celo.org',
+    },
   },
   mbConfig: {
     apiKey: adminApiKey,
     host: deploymentEndpoint,
     allowUpdateAddress: ['development', 'testing'],
     allowUpdateContract: ['development'],
+  },
+  etherscan: {
+    apiKey: {
+      alfajores: process.env.CELOSCAN_API_KEY ?? '',
+      celo: process.env.CELOSCAN_API_KEY ?? '',
+    },
+    customChains: [
+      {
+        chainId: 44_787,
+        network: 'alfajores',
+        urls: {
+          apiURL: 'https://api-alfajores.celoscan.io/api',
+          browserURL: 'https://alfajores.celoscan.io',
+        },
+      },
+      {
+        chainId: 42_220,
+        network: 'celo',
+        urls: {
+          apiURL: 'https://api.celoscan.io/api',
+          browserURL: 'https://celoscan.io/',
+        },
+      },
+    ],
+  },
+  sourcify: {
+    enabled: false,
   },
   solidity: {
     compilers: [
